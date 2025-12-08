b0eb0852cd28ad414cf15c47f9318941b8e9362d
Long Nguyen <long.nguyen@codelink.io>
2025-12-08 10:11:03 +0000

    feat(backend): establish orm database connection

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,25 @@
+import { DogsModule } from "@/modules/domain/dogs/dogs.module";
 import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { DogsModule } from "@/modules/domain/dogs/dogs.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-	imports: [DogsModule],
+	imports: [
+		// Starter Modules
+		ConfigModule.forRoot(),
+		SequelizeModule.forRoot({
+			dialect: "oracle",
+			host: "localhost",
+			port: 1521,
+			username: "SYSTEM",
+			password: "<your_password>",
+			database: "XE",
+		}),
+		// Domain Modules
+		DogsModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { Sequelize } from "sequelize-typescript";
 
 @Injectable()
 export class AppService {
+	constructor(private readonly sequelize: Sequelize) {
+		this.sequelize.authenticate().catch(() => {
+			console.error("Unable to connect to the database:");
+		});
+	}
+
 	getHello(): string {
 		return "Hello World!";
 	}
