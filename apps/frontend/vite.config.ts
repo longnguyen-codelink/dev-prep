84d3ab27bb0d69be1b8f1a6413f015d7564cdcb6
Long Nguyen <long.nguyen@codelink.io>
2025-12-08 03:44:11 +0000

    fix(frontend): init shadcn

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		host: "127.0.0.1",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
 	},
 });
